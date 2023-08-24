d2d80b9478cf27b41a11d332390fb350a82402ab
Dawit <yepdawit@gmail.com>
2023-08-24 16:18:24 -0400

    fixed database js

--- db/videoGames.js
+++ db/videoGames.js
@@ -1,63 +1,107 @@
-const client = require('./client');
-const util = require('util');
+const client = require("./client");
+const util = require("util");
 
 /* const REPLACE_ME = 'HELP REPLACE ME!!!!'; */
 
 // GET - /api/video-games - get all video games
 async function getAllVideoGames() {
-    try {
-        const { rows: videoGames } = await client.query(`SELECT * FROM videoGames;`);
-        return videoGames;
-    } catch (error) {
-        throw new Error("Could not get video games)
-    }
+  try {
+    const { rows: videoGames } = await client.query(
+      `SELECT * FROM videoGames;`
+    );
+    return videoGames;
+  } catch (error) {
+    throw new Error("Could not get video games");
+  }
 }
 
 // GET - /api/video-games/:id - get a single video game by id
 async function getVideoGameById(id) {
-    try {
-        const { rows: [videoGame] } = await client.query(`
+  try {
+    const {
+      rows: [videoGame],
+    } = await client.query(
+      `
             SELECT * FROM videoGames
             WHERE id = $1;
-        `, [id]);
-        return videoGame;
-    } catch (error) {
-        throw error;
-    }
+        `,
+      [id]
+    );
+    return videoGame;
+  } catch (error) {
+    throw error;
+  }
 }
 
 // POST - /api/video-games - create a new video game
 async function createVideoGame(body) {
-    const { name, description, price, rating, "inStock" } = body;
-    try {
-        const { rows: [videoGame] } = await client.query(`
+  const { name, description, price, rating, inStock } = body;
+  try {
+    const {
+      rows: [videoGame],
+    } = await client.query(
+      `
             INSERT INTO videoGames(name, description, price, rating, "inStock")
             VALUES($1, $2, $3, $4, $5)
             RETURNING *;
-        `, [name, description, price, rating, "inStock"]);
-        return videoGame;
-    } catch (error) {
-        throw error;
-    }
-
+        `,
+      [name, description, price, rating, inStock]
+    );
+    return videoGame;
+  } catch (error) {
+    throw error;
+  }
 }
-
-
 
 // PUT - /api/video-games/:id - update a single video game by id
 async function updateVideoGame(id, fields = {}) {
-    const setString = Object.keys(fields).map(key, index) 
+  const setString = Object.keys(fields)
+    .map(key, (index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+  if (setString.length === 0) {
+    return;
+  }
+  try {
+    const {
+      rows: [videoGame],
+    } = await client.query(
+      `
+            UPDATE videoGames
+            SET ${setString}
+            WHERE id=${id}
+            RETURNING *;
+        `,
+      Object.values(fields)
+    );
+    return videoGame;
+  } catch (error) {
+    throw error;
+  }
 }
 
 // DELETE - /api/video-games/:id - delete a single video game by id
 async function deleteVideoGame(id) {
-    // LOGIC GOES HERE
+  try {
+    const {
+      rows: [videoGame],
+    } = await client.query(
+      `
+            DELETE FROM videoGames
+            WHERE id=$1
+            RETURNING *;
+        `,
+      [id]
+    );
+    return videoGame;
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = {
-    getAllVideoGames,
-    getVideoGameById,
-    createVideoGame,
-    updateVideoGame,
-    deleteVideoGame
-}
+  getAllVideoGames,
+  getVideoGameById,
+  createVideoGame,
+  updateVideoGame,
+  deleteVideoGame,
+};
